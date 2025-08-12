1aedf1101ab339907ea4e2e02d4c60fd5c547115
@iamsaphal <81309102+success-glich@users.noreply.github.com>
2025-08-12 23:04:27 +0545

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -14,7 +14,7 @@ export default function Home() {
       ) : (
         <div>
           <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
-            Nothing. hi this is rohan
+            Nothing. hi this is safal project
           </p>
         </div>
       )}
